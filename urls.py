6b7de28503337acfd8a8687fec29165d59c539fe
KaveDev <87555836+Sukerutoon@users.noreply.github.com>
2022-09-30 20:05:09 -0500

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app.views.Tarea import Works
-from app.views.Cliente import Cliente
-
-urlpatterns = [
-    path('ingresar/', Cliente.as_view()),
-    path('tareas/', Works.as_view()),
-]
